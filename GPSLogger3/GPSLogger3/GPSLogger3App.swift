d2aa219bbf2003d1f2350c0937a25643ea3d220d
YA <9039417+yzkn@users.noreply.github.com>
2024-08-02 12:35:39 +0900

    init

---
+++ GPSLogger3/GPSLogger3/GPSLogger3App.swift
@@ -0,0 +1,17 @@
+//
+//  GPSLogger3App.swift
+//  GPSLogger3
+//
+//  Created by Yu on 2024/08/02.
+//
+
+import SwiftUI
+
+@main
+struct GPSLogger3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
